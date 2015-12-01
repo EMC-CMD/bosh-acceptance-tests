b6728dcbc07ca95726b2727bda75ea6426ee8cc8
Megan Murawski <megan.hyland@emc.com>
2015-12-01 14:24:37 -0500

    Put core tag for all supported tests by rackhd
    [#102599582](https://www.pivotaltracker.com/story/show/102599582)
    
    Signed-off-by: Xuebin He <xuebin.he@emc.com>

--- spec/system/network_configuration_spec.rb
+++ spec/system/network_configuration_spec.rb
@@ -1,6 +1,6 @@
 require 'system/spec_helper'
 
-describe 'network configuration' do
+describe 'network configuration', core: true do
   before(:all) do
     @requirements.requirement(@requirements.stemcell)
     @requirements.requirement(@requirements.release)
